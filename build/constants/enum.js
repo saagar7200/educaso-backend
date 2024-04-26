304d9b7bd7f8265fd7fd919a3b70571c9dfdaf25
Sagar Bhandari <rsaagar7200@gmail.com>
2024-04-26 22:14:40 +0545

    build

--- build/constants/enum.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Gender = void 0;
-var Gender;
-(function (Gender) {
-    Gender["MALE"] = "MALE";
-    Gender["FEMALE"] = "FEMALE";
-    Gender["OTHERS"] = "OTHERS";
-})(Gender || (exports.Gender = Gender = {}));
